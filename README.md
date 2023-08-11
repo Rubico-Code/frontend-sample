786ac6826c4b68567d7a8b6bc3fda595973ff166
Rubico <network@rubicotech.in>
2023-08-11 10:42:46 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rubico CodeSamples
+
+This repo includes the code samples for front-end technologies.
+
+Please do not include any sensitive information in this repository.
---
+++ media-slider/js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    // Slider
+    $('.generic-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        cssEase: 'linear',
+        slideToShow: 1,
+        slideToScroll: 1,
+        arrows: false,
+        autoplay: true,
+    });
+});
